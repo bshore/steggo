d2942e61e62d7ae5bbeaa546414c6719ad60e93a
Brandon <shore7@gmail.com>
2019-09-03 20:14:21 -0500

    jpeg and png embedding working and not as noticable

--- pkg/process/embed.go
+++ pkg/process/embed.go
@@ -14,11 +14,11 @@ import (
 func EmbedMsgInImage(msg, format string, file image.Image) (draw.Image, error) {
 	var bitsIndex int
 	var err error
-	var newR, newG, newB uint8
+	var newR, newG, newB, newA uint16
 	bitArr := BreakupMessageBytes(msg)
 	bitMax := len(bitArr) - 1
 	bounds := file.Bounds()
-	newFile := image.NewRGBA(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
+	newFile := image.NewRGBA64(image.Rect(0, 0, bounds.Dx(), bounds.Dy()))
 	// For each vertical row
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		// For each pixel in each row
@@ -26,54 +26,54 @@ func EmbedMsgInImage(msg, format string, file image.Image) (draw.Image, error) {
 			r, g, b, a := file.At(x, y).RGBA()
 			// If the iteration is still under the length of message bits
 			if bitsIndex < bitMax {
-				newR, err = embedInColor(bitArr[bitsIndex], uint8(r))
+				newR, err = embedInColor(bitArr[bitsIndex], r)
 				if err != nil {
 					return nil, err
 				}
 				// Check if there is a next bit to embed
 				if bitsIndex+1 < bitMax {
-					newG, err = embedInColor(bitArr[bitsIndex+1], uint8(g))
+					newG, err = embedInColor(bitArr[bitsIndex+1], g)
 					if err != nil {
 						return nil, err
 					}
 					// Check if there is a next bit to embed
 					if bitsIndex+2 < bitMax {
-						newB, err = embedInColor(bitArr[bitsIndex+2], uint8(b))
+						newB, err = embedInColor(bitArr[bitsIndex+2], b)
 						if err != nil {
 							return nil, err
 						}
 						// Check if there is a next bit to embed
-						// if bitsIndex+3 < bitMax {
-						// 	newA, err = embedInColor(bitArr[bitsIndex+3], uint8(a))
-						// 	if err != nil {
-						// 		return nil, err
-						// 	}
-						// } else {
-						// 	// No more message bits to embed, copy color value
-						// 	newA = uint8(a)
-						// }
+						if bitsIndex+3 < bitMax {
+							newA, err = embedInColor(bitArr[bitsIndex+3], a)
+							if err != nil {
+								return nil, err
+							}
+						} else {
+							// No more message bits to embed, copy color value
+							newA = uint16(a)
+						}
 					} else {
 						// No more message bits to embed, copy color value
-						newB = uint8(b)
+						newB = uint16(b)
 					}
 				} else {
 					// No more message bits to embed, copy color value
-					newG = uint8(g)
+					newG = uint16(g)
 				}
 			} else {
 				// No more message bits to embed, just copy the rest of the pixels
-				newR = uint8(r)
-				newG = uint8(g)
-				newB = uint8(b)
-				// newA = uint8(a)
+				newR = uint16(r)
+				newG = uint16(g)
+				newB = uint16(b)
+				newA = uint16(a)
 			}
-			newColor := color.RGBA{
+			newColor := color.RGBA64{
 				R: newR,
 				G: newG,
 				B: newB,
-				A: uint8(a),
+				A: newA,
 			}
-			newFile.SetRGBA(x, y, newColor)
+			newFile.SetRGBA64(x, y, newColor)
 			bitsIndex = bitsIndex + 4
 		}
 	}
@@ -86,19 +86,19 @@ func EmbedMsgInGIF(msg, format string, file *gif.GIF) (draw.Image, error) {
 	return nil, nil
 }
 
-// There's gotta be a better way...
-func embedInColor(a string, b uint8) (uint8, error) {
+func embedInColor(a []string, b uint32) (uint16, error) {
 	colorVal := strconv.FormatUint(uint64(b), 2)
 	colorBits := strings.Split(ZeroPadLeft(colorVal), "")
-	// Last Bit becomes the bit of encoded msg
-	colorBits[len(colorBits)-1] = a
+	// Least Significant Bits become bit pair of encoded msg
+	colorBits[len(colorBits)-2] = a[0]
+	colorBits[len(colorBits)-1] = a[1]
 
 	// Rejoin & reparse the new color values
 	colorStr := strings.Join(colorBits, "")
-	newColor, err := strconv.ParseUint(colorStr, 10, 64)
+	newColor, err := strconv.ParseUint(colorStr, 2, 64)
 	if err != nil {
 		return 0, err
 	}
 
-	return uint8(newColor), nil
+	return uint16(newColor), nil
 }
--- pkg/process/message.go
+++ pkg/process/message.go
@@ -22,16 +22,24 @@ func ZeroPadLeft(str string) string {
 // BreakupMessageBytes busts apart each message byte into
 // string arrays of two for swapping Least Significant Bits
 // of the image file's writable bytes
-func BreakupMessageBytes(msg string) []string {
+func BreakupMessageBytes(msg string) [][]string {
 	msgBytes := []byte(msg)
-	var bitArr []string
+	var bitArr [][]string
 	for _, b := range msgBytes {
 		// Kinda dumb, I'm sure there's a better way to
 		// get a binary representation of a byte...
 		binStr := strconv.FormatInt(int64(b), 2)
 		bits := strings.Split(ZeroPadLeft(binStr), "")
-		for _, bit := range bits {
-			bitArr = append(bitArr, bit)
+		for i, bit := range bits {
+			// Current iteration is odd ?
+			// Grab bits in pairs (01, 23, 45, 67)
+			if i%2 != 0 && i != 0 {
+				// 00000010 two's bit
+				two := bit
+				// 00000001 one's bit
+				one := bits[i-1]
+				bitArr = append(bitArr, []string{two, one})
+			}
 		}
 	}
 	return bitArr
